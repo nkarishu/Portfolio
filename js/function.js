2cf4b40f81724e5cf825190b5d8011caa0c4f583
nkarishu <86212360+nkarishu@users.noreply.github.com>
2021-09-22 10:25:28 +0900

    new

--- js/function.js
+++ js/function.js
@@ -1,13 +1,13 @@
-$(function(){
+$(function () {
 
     const viewHeight = $(window).height();
     const colors = ['#fff', '#fff', '#707070', '#000'];
 
-    $(window).on('scroll',function(){
-        const ST =  $(window).scrollTop();
+    $(window).on('scroll', function () {
+        const ST = $(window).scrollTop();
 
         //bg color change
-        $('section').each(function(){
+        $('section').each(function () {
             const target = $(this).offset().top;
             const index = $(this).index();
 
@@ -16,6 +16,99 @@ $(function(){
             }
         });
 
+    });
+
+    $(function () {
+        const target1 = $("#parallax1");
+        const targetPosOT1 = target1.offset().top;
+        const targetFactor = 0.1;
+        const windowH = $(window).height();
+        const scrollYStart1 = targetPosOT1 - windowH;
+        $(window).on('scroll', function () {
+            const scrollY = $(this).scrollTop();
+            if (scrollY > scrollYStart1) {
+                target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+            } 
+        });
+    });
+
+    $(function () {
+        const target1 = $("#parallax2");
+        const targetPosOT1 = target1.offset().top;
+        const targetFactor = 0.1;
+        const windowH = $(window).height();
+        const scrollYStart1 = targetPosOT1 - windowH;
+        $(window).on('scroll', function () {
+            const scrollY = $(this).scrollTop();
+            if (scrollY > scrollYStart1) {
+                target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+            } 
+        });
+    });
+    $(function () {
+        const target1 = $("#parallax3");
+        const targetPosOT1 = target1.offset().top;
+        const targetFactor = 0.1;
+        const windowH = $(window).height();
+        const scrollYStart1 = targetPosOT1 - windowH;
+        $(window).on('scroll', function () {
+            const scrollY = $(this).scrollTop();
+            if (scrollY > scrollYStart1) {
+                target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+            } 
+        });
+    });
+    $(function () {
+        const target1 = $("#parallax4");
+        const targetPosOT1 = target1.offset().top;
+        const targetFactor = 0.1;
+        const windowH = $(window).height();
+        const scrollYStart1 = targetPosOT1 - windowH;
+        $(window).on('scroll', function () {
+            const scrollY = $(this).scrollTop();
+            if (scrollY > scrollYStart1) {
+                target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+            } 
+        });
+    });
+    $(function () {
+        const target1 = $("#parallax5");
+        const targetPosOT1 = target1.offset().top;
+        const targetFactor = 0.1;
+        const windowH = $(window).height();
+        const scrollYStart1 = targetPosOT1 - windowH;
+        $(window).on('scroll', function () {
+            const scrollY = $(this).scrollTop();
+            if (scrollY > scrollYStart1) {
+                target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+            } 
+        });
+    });
+    $(function () {
+        const target1 = $("#parallax6");
+        const targetPosOT1 = target1.offset().top;
+        const targetFactor = 0.1;
+        const windowH = $(window).height();
+        const scrollYStart1 = targetPosOT1 - windowH;
+        $(window).on('scroll', function () {
+            const scrollY = $(this).scrollTop();
+            if (scrollY > scrollYStart1) {
+                target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+            } 
+        });
+    });
+    $(function () {
+        const target1 = $("#parallax7");
+        const targetPosOT1 = target1.offset().top;
+        const targetFactor = 0.1;
+        const windowH = $(window).height();
+        const scrollYStart1 = targetPosOT1 - windowH;
+        $(window).on('scroll', function () {
+            const scrollY = $(this).scrollTop();
+            if (scrollY > scrollYStart1) {
+                target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+            } 
+        });
     });
 
 });
